1785d5a8f17637ebb9ebd7718f91e5a99dec17c2
Jeff Brown <Jeffreydylanbrown@gmail.com>
2020-02-21 10:34:27 -0800

    maps key updated for debug and release

--- app/src/main/java/com/example/jeff/iplocator/util/App.kt
+++ app/src/main/java/com/example/jeff/iplocator/util/App.kt
@@ -3,10 +3,11 @@ package com.example.jeff.iplocator.util
 import android.app.Application
 import com.example.jeff.iplocator.network.IPAddressAPIService
 import org.koin.android.ext.koin.androidContext
-import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
-import org.koin.core.logger.Level
+import timber.log.Timber
 
+
+//class to start Koin
 class App : Application() {
 
     private lateinit var ipAddressAPIService: IPAddressAPIService
@@ -14,7 +15,7 @@ class App : Application() {
         super.onCreate()
         //call to start Koin
         startKoin {
-            androidLogger(Level.DEBUG)
+            Timber.i("Koin started")
             androidContext(this@App)
             modules(listOf(
                 networkModule,
--- app/src/main/java/com/example/jeff/iplocator/view/MainActivity.kt
+++ app/src/main/java/com/example/jeff/iplocator/view/MainActivity.kt
@@ -25,6 +25,7 @@ class MainActivity : AppCompatActivity() {
             Timber.d("Timber Started")
 
         }
+
     }
 
 
